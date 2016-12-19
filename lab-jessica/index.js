9cd0fe607db7983565e691185056a62d060214eb
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2016-12-18 20:19:12 -0800

    Moved createErrors to own module

--- lab-jessica/index.js
+++ lab-jessica/index.js
@@ -3,26 +3,20 @@
 const express = require('express');
 const morgan = require('morgan');
 const jsonParser = require('body-parser').json();
-
-const createError = require('http-errors');
+const createError = require('./lib/create-errors.js');
 
 const app = express();
 const router = express.Router();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(morgan('dev'));
 app.use(jsonParser);
 
-app.use((err, req, res, next) => {
-  console.error(err.message);
-
-  err = createError(500, err.message);
-  res.status(err.status).send(err.name);
-  next();
-});
+app.use(createError);
 
 require('./routes/book-routes')(router);
 app.use(router);
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function() {
   console.log('server listening on port', PORT);
